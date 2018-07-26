4853e96204469dae4c6c4712c2bc1a288afac9d6
Choong Yee Ching <rainchoong@gmail.com>
2018-07-26 18:19:36 +0800

    Initial Commit

---
+++ Hello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Choong Yee Ching on 26/07/2018.
+//  Copyright © 2018 Choong Yee Ching. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
